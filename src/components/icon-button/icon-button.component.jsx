00be32037f51b851a1f2c25796f5cd6a9348c9ec
Manu <manu035@gmail.com>
2020-11-08 15:28:56 +0100

    correction nom icon-button

--- src/components/icon-button/icon-button.component.jsx
+++ src/components/icon-button/icon-button.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './icon-buttom.styles.scss';
+import './icon-button.styles.scss';
 
 const IconButton = ({ icon, onClick, title }) => (
   <span onClick={onClick} className='icon-button' alt={title} title={title}>
